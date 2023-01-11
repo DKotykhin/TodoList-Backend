e10c3fc9f0706742e0a4c64fd45ec38688e537fa
Dmytro <kotykhin_d@ukr.net>
2023-01-11 20:11:16 +0100

    add landing page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
+import favicon from 'serve-favicon';
 
 import router from './router/router.js';
 import { errorHandler } from "./middlewares/index.js";
@@ -14,12 +16,15 @@ mongoose
     .catch((err) => console.log('DB Error:', err))
 
 const app = express();
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/upload', express.static('uploads'));
 app.use('/api', router);
+app.use(express.static(__dirname + '/src/public'));
+app.use(favicon(path.join(__dirname, 'src/public', 'favicon.ico')))
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 4001;
